177f7befcc2c956e00ef57846ae5726089b3963f
Gautam Narayan <98248099+gnarayan1337@users.noreply.github.com>
2024-12-15 16:13:24 -0600

    Update GlassCTA.js

--- app/Components/GlassCTA.js
+++ app/Components/GlassCTA.js
@@ -98,7 +98,10 @@ const AnimatedCard = () => {
     >
       <h2 className="text-white text-4xl font-semibold text-center">Transform Learning with AI</h2>
       <p className="text-white mt-4 text-center sm:px-16 text-gray-300">
-  Sign up with your school-affiliated email and join a community of over 20,000 educators & students! <br/> -Gautam Narayan, Founder
+        Sign up with your school-affiliated email and join a community of over 20,000 educators & students!
+      </p>
+      <p className="text-gray-300 mt-6 text-center italic text-lg">
+        - Gautam Narayan, Founder
       </p>
       <div className="max-w-sm mx-auto">
         <WaitlistForm />
@@ -106,6 +109,35 @@ const AnimatedCard = () => {
     </motion.div>
   );
 };
+
+
+// const AnimatedCard = () => {
+//   const controls = useAnimation();
+
+//   useEffect(() => {
+//     controls.start({
+//       opacity: 1,
+//       scale: 1,
+//       transition: { delay: 0.6, duration: 0.5, type: "spring", stiffness: 100 },
+//     });
+//   }, [controls]);
+
+//   return (
+//     <motion.div
+//       initial={{ opacity: 0, scale: 0.5 }}
+//       animate={controls}
+//       className="bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg border border-gray-500 rounded-3xl p-8 py-12 shadow-lg max-w-2xl w-full mb-6"
+//     >
+//       <h2 className="text-white text-4xl font-semibold text-center">Transform Learning with AI</h2>
+//       <p className="text-white mt-4 text-center sm:px-16 text-gray-300">
+//   Sign up with your school-affiliated email and join a community of over 20,000 educators & students! <br/> -Gautam Narayan, Founder
+//       </p>
+//       <div className="max-w-sm mx-auto">
+//         <WaitlistForm />
+//       </div>
+//     </motion.div>
+//   );
+// };
 
 const WaitlistForm = () => {
   const inputRef = useRef(null);
